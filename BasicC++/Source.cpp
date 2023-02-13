63aaff183f3d9dda76930c3067d4069f0e732d7c
Коля <@DESKTOP-3O0178S>
2023-02-13 12:34:36 +0200

    while

--- BasicC++/Source.cpp
+++ BasicC++/Source.cpp
@@ -1,11 +1,34 @@
-#include<iostream>
-#include<ctime>
-#include<string>
-#include<algorithm>
+﻿include<iostream>
 
 using namespace std;
 
 int main()
 {
+	int nam1, nam2;
+	setlocale(LC_ALL, "ukr");
+	srand(time(0));
+	cout << "Íå ìåíøå ";
+	cin >> nam1;
+	cout << "Íå á³ëüøå ";
+	cin >> nam2;
+	int nam;
+	cin >> nam;
+	while (nam)
+	{
+
+		if (nam1 || nam2)
+		{
+			cout << " ä³àïàçîí ";
+			if (nam < nam1)
+				cout << "+" << endl;
+			else if (nam > nam2)
+				cout << "-" << endl;
+			else
+				cout << "win" << endl;
+			system("pause");
+			system("cls");
+			return 0;
+		}
+	}
 	return 0;
 }
